25bf2378616caf845b3ae0cf453eb94242632ab6
Nima <nimiologyy@gmail.com>
2021-08-01 22:54:35 +0430

    Start

--- StaticPages/urls.py
+++ StaticPages/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import Home,About
 
-app_name = 'Projects'
+app_name = 'StaticPages'
 urlpatterns = [
     path("",Home, name="main"),
     path("about", About, name="About"),
