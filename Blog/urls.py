df3f71d471cb8bade2a36a2da2fd5f12b56bf92c
Nima <nimiologyy@gmail.com>
2021-08-07 22:20:35 +0430

    commit

--- Blog/urls.py
+++ Blog/urls.py
@@ -4,7 +4,7 @@ app_name = 'Blog'
 urlpatterns = [
     path("", articles, name="blog"),
     path("page/<int:page>", articles, name="blog"),
-    path("<slug:slug>", Blog, name="article"),
-    path("category/<slug:slug>", Categorygetter, name='category'),
-    path("category/<slug:slug>/page/<int:page>", Categorygetter, name='category'),
+    path("/<slug:slug>", Blog, name="article"),
+    path("/category/<slug:slug>", Categorygetter, name='category'),
+    path("/category/<slug:slug>/page/<int:page>", Categorygetter, name='category'),
 ]
