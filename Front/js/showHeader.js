f384c001463daf19801c11a3a553f64a8b47995c
Mansour <marzban.dev@gmail.com>
2021-08-16 22:57:19 +0430

    Commit all changes :/

--- Front/js/showHeader.js
+++ Front/js/showHeader.js
@@ -1,18 +1,23 @@
 docReady(function () {
 
     // Run navbar animation
-    setTimeout(function () {
-        var headerEl = document.getElementsByTagName('header')[0];
-        console.log(document.getElementsByTagName('header'));
+    var headerEl = document.getElementsByTagName('header')[0];
+
+    if (WITH_DELAY) {
+        setTimeout(function () {
+            headerEl.classList.add('header-faded-in');
+        }, 1000);
+    } else {
         headerEl.classList.add('header-faded-in');
-    }, 1000);
+    }
+
 
     var searchBtn = document.getElementsByClassName('navbar-search-btn')[0];
     var searchBoxModal = document.getElementsByClassName('search-box-modal')[0];
 
-    var searchBoxModalInputBtn =  document.getElementsByClassName('search-box-modal-input')[0].children[1];
-    var searchBoxModalContainer =  document.getElementsByClassName('search-box-modal-container')[0];
-    var searchBoxModalInputLine =  document.getElementsByClassName('search-box-modal-line')[0];
+    var searchBoxModalInputBtn = document.getElementsByClassName('search-box-modal-input')[0].children[1];
+    var searchBoxModalContainer = document.getElementsByClassName('search-box-modal-container')[0];
+    var searchBoxModalInputLine = document.getElementsByClassName('search-box-modal-line')[0];
 
     searchBtn.addEventListener('click', function () {
 
@@ -21,17 +26,17 @@ docReady(function () {
             searchBtn.classList.toggle('navbar-search-btn-active');
             searchBoxModalContainer.style.opacity = '0';
             searchBoxModalInputLine.style.width = '0';
-            Object.assign(searchBoxModalInputBtn.style,{opacity:'0',bottom:'-20px'});
+            Object.assign(searchBoxModalInputBtn.style, {opacity: '0', bottom: '-20px'});
         } else {
             searchBtn.children[0].classList.replace('bi-search', 'bi-x-lg');
             searchBtn.classList.toggle('navbar-search-btn-active');
-            setTimeout(function (){
+            setTimeout(function () {
                 searchBoxModalContainer.style.opacity = '1';
                 searchBoxModalInputLine.style.width = '100%';
-            },750);
-            setTimeout(function (){
-                Object.assign(searchBoxModalInputBtn.style,{opacity:'1',bottom:'0'});
-            },1250)
+            }, 750);
+            setTimeout(function () {
+                Object.assign(searchBoxModalInputBtn.style, {opacity: '1', bottom: '0'});
+            }, 1250)
         }
 
         searchBoxModal.classList.toggle('search-box-modal-active');
