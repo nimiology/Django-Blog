b02747a15d1f0a5c6df8cbdae2c21e6f3ca0aec9
nima <nimiologyy@gmail.com>
2021-10-23 20:18:16 +0330

    paginator

--- Blog/views.py
+++ Blog/views.py
@@ -15,6 +15,7 @@ def Blog(request, page=1):
     article_list = Article.objects.filter(published=True)
     pagintor = Paginator(article_list, 3)
     article = pagintor.get_page(page)
+    print(article)
     context = {
         "articles": article,
         "categories": Category.objects.filter(status=True).order_by('-id')[:5],
@@ -27,7 +28,7 @@ def Blog(request, page=1):
 def CategoryView(request, slug, page=1):
     Categorys = get_object_or_404(Category, slug=slug, status=True)
     article_list = Categorys.article.filter(published=True)
-    pagintor = Paginator(article_list, 6)
+    pagintor = Paginator(article_list, 3)
     article = pagintor.get_page(page)
     context = {
         "category": Categorys,
@@ -41,7 +42,7 @@ def CategoryView(request, slug, page=1):
 def TagView(request, slug, page=1):
     tag = get_object_or_404(Tag, slug=slug, status=True)
     article_list = tag.article.filter(published=True)
-    pagintor = Paginator(article_list, 6)
+    pagintor = Paginator(article_list, 3)
     article = pagintor.get_page(page)
     context = {
         "category": tag,
